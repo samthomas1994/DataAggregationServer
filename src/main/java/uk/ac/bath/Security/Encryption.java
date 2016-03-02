97e2ed1e136a3503a68603fd925f4ce9a24e0988
Sam Thomas <sthomas.gb@gmail.com>
2016-03-02 14:22:00 +0000

    Fixed weird merge stuff

--- src/main/java/uk/ac/bath/Security/Encryption.java
+++ src/main/java/uk/ac/bath/Security/Encryption.java
@@ -1,6 +1,5 @@
 package uk.ac.bath.Security;
 
-<<<<<<< HEAD
 import sun.misc.BASE64Encoder;
 
 import java.io.UnsupportedEncodingException;
@@ -30,10 +29,4 @@ public final class Encryption {
         String encryptedPassword = (new BASE64Encoder()).encode(raw);
         return encryptedPassword;
     }
-=======
-/**
- * Created by Sam on 25/02/2016.
- */
-public class Encryption {
->>>>>>> 3d795b12caa70a6565a8a6715888accb7a676143
 }
--- src/main/java/uk/ac/bath/Security/ValidInput.java
+++ src/main/java/uk/ac/bath/Security/ValidInput.java
@@ -1,16 +1,11 @@
 package uk.ac.bath.Security;
 
-<<<<<<< HEAD
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-=======
->>>>>>> 3d795b12caa70a6565a8a6715888accb7a676143
 /**
  * Created by Sam on 25/02/2016.
  */
 public class ValidInput {
-<<<<<<< HEAD
 
     public boolean validateName(String txt) {
         String regx = "^[\\p{L} .'-]+$";
@@ -25,6 +20,4 @@ public class ValidInput {
         Matcher matcher = pattern.matcher(txt);
         return matcher.find();
     }
-=======
->>>>>>> 3d795b12caa70a6565a8a6715888accb7a676143
 }
